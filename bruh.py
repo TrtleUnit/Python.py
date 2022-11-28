7d7194abd121c5159809abb953829ce71b443f91
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2022-11-28 10:12:02 +0100

    Add files via upload

---
+++ bruh.py
@@ -0,0 +1,21 @@
+
+#Rekenmachine
+
+Getal1 = float(input("Voer uw eerste getal in: "))
+Bewerking = (input ("Welke bewerking wilt u uitvoeren"))
+Getal2 = float(input("Voer uw tweede getal in: "))
+
+if Bewerking == "+":
+    print(Getal1 + Getal2)
+    
+elif Bewerking == "-":
+    print(Getal1 - Getal2)
+    
+elif Bewerking == "x":
+    print(Getal1 * Getal2)
+
+elif Bewerking == ":":
+    print(Getal1 / Getal2)
+  
+
+else: print("Deze functie werekt niet.")
