d161fb9c216f2e214434449043a916d4f1093de0
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2022-11-30 12:59:29 +0100

    Update bruh.py

--- bruh.py
+++ bruh.py
@@ -1,21 +1,30 @@
 
 #Rekenmachine
 import math
-Getal1 = float(input("Voer uw eerste getal in: "))
-Bewerking = (input ("Welke bewerking wilt u uitvoeren"))
-Getal2 = float(input("Voer uw tweede getal in: "))
+while True:
+        Getal1 = float(input("Voer uw eerste getal in: "))
+        Bewerking = (input ("Welke bewerking wilt u uitvoeren"))
+        Getal2 = float(input("Voer uw tweede getal in: "))
 
-if Bewerking == "+":
-    print(Getal1 + Getal2)
+        if Bewerking == "+":
+            print(Getal1 + Getal2)
     
-elif Bewerking == "-":
-    print(Getal1 - Getal2)
+        elif Bewerking == "-":
+            print(Getal1 - Getal2)
     
-elif Bewerking == "x":
-    print(Getal1 * Getal2)
+        elif Bewerking == "x":
+            print(Getal1 * Getal2)
 
-elif Bewerking == ":":
-    print(Getal1 / Getal2)
+        elif Bewerking == ":":
+            print(Getal1 / Getal2)
+        
+        else: print("Deze functie werekt niet.")
+        cont = input("Continue?y/n:")
+        if cont == "n": break
+        if cont == "y": continue
+       
+       
 
-antwoord = Getal1 + Bewerking + Getal2
-print (antwoord)
+
+
+
