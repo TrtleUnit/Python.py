0438d6666f8b4eef126425c9cbf024970ba7e7db
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2022-12-08 11:40:47 +0100

    Create loopdice.py

---
+++ loopdice.py
@@ -0,0 +1,15 @@
+import random
+hoeveel = input("Hoeveel dobbelstenen wilt u rollen?")
+repeat = "Y"
+
+while repeat == "Y":
+    print("Het resultaat:")
+    print(random.randint(1,6))
+
+    repeat =input("Do you wanna roll again Y/N?")
+    if repeat == "Y": continue
+    if repeat == "N": break
+    else: print ("Deze functie werkt niet. Begin opnieuw.")
+    
+    
+    
